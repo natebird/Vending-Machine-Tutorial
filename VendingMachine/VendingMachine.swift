4083f069d80c50a161577045052a08b96935f40b
Nate Bird <natebird@gmail.com>
2016-03-04 10:17:27 -0500

    Add ErrorTypes and InventoryUnarchiver class.

--- VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine.swift
@@ -21,14 +21,62 @@ protocol VendingMachineType {
 }
 
 protocol ItemType {
-  var price: Double { get }
+  let price: Double { get }
   var quantity: Double { get set }
 }
 
 
+// Error Types
+
+enum InventoryError: ErrorType {
+  case invalidResource
+  case conversionError
+  case invalidKey
+}
+
+// Helper Classes
+class PlistConverter {
+  class func dictionaryFromFile(resource: String, ofType type: String) throws -> [String: AnyObject] {
+    guard let path = NSBundle.mainBundle().pathForResource(resource, ofType: type) else {
+      throw InventoryError.invalidResource
+    }
+    
+    guard let dictionary = NSDictionary(contentsOfFile: path),
+    let castDictionary = dictionary as? [String: AnyObject] else {
+      throw InventoryError.conversionError
+    }
+    
+    return castDictionary
+  }
+}
+
+class InventoryUnarchiver {
+  class func vendingInventoryFromDictionary(dictionary: [String: AnyObject]) throws -> [VendingSelection: ItemType] {
+    
+    var inventory: [VendingSelection : ItemType] = [:]
+    
+    for (key, value) in dictionary {
+      if let itemDict = value as? [String : Double],
+         let price = itemDict["price"],
+         let quantity = itemDict["quantity"] {
+        
+          let item = VendingItem(price: price, quantity: quantity)
+        
+          guard let key = VendingSelection(rawValue: key) else {
+            throw InventoryError.invalidKey
+          }
+        
+        inventory.updateValue(item, forKey: key)
+      }
+      
+      return inventory
+    }
+  }
+}
+
 // Concrete Types
 
-enum VendingSelection {
+enum VendingSelection: String {
   case Soda
   case DietSoda
   case Chips
