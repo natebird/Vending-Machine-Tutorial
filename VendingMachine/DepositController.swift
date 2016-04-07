9c2dfb9aec068868a7e6355f71f0b1a105195e12
Nate Bird <natebird@gmail.com>
2016-04-07 10:21:35 -0400

    Add deposit controller and modal segue

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,25 @@
+//
+//  DepositController.swift
+//  VendingMachine
+//
+//  Created by Nate Bird on 4/7/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  @IBAction func dismiss() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+
+}
